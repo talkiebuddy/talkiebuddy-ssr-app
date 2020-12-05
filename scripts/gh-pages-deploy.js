42edfcc80ad6cae34c4d6f91bb78c1e1294f7295
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2020-12-05 20:42:52 +0700

    change pages to storybook

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -10,12 +10,19 @@ const e_rocket = emoji.get('rocket');
   try {
     await execa('git', ['checkout', '--orphan', 'gh-pages'])
     console.log(`${e_arrows} ${chalk.yellow('Building...')}`)
-    await execa('npm', ['run', 'build'])
-    await execa('npm', ['run', 'generate'])
+    // To build Storybook
+    await execa('npm', ['run', 'storybook:build'])
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync('dist') ? 'dist' : 'build'
+    const folderName = fs.existsSync('storybook-static') ? 'storybook-static' : ''
     await execa('git', ['--work-tree', folderName, 'add', '--all'])
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages'])
+    // // To build App
+    // await execa('npm', ['run', 'build'])
+    // await execa('npm', ['run', 'generate'])
+    // // Understand if it's dist or build folder
+    // const folderName = fs.existsSync('dist') ? 'dist' : 'build'
+    // await execa('git', ['--work-tree', folderName, 'add', '--all'])
+    // await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages'])
     console.log(`${e_arrows} ${chalk.yellow('Pushing...')}`)
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
     await execa('rm', ['-r', folderName])
